df73c36b50637fe0989e3330d359edb15a6e0200
miguelh612 <mi.villagra@duocuc.cl>
2026-05-07 18:55:23 +0000

    2.4 vpc actualizada

--- EA2/ACT2.1/s3_module/main.tf
+++ EA2/ACT2.1/s3_module/main.tf
@@ -38,3 +38,11 @@ resource "aws_s3_bucket_policy" "public_read" {
     ]
   })
 }
+
+resource "aws_s3_bucket_ownership_controls" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
