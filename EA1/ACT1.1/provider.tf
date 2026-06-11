46656bb98c1165e357e532deddc3bf157f8f97cb
miguelh612 <mi.villagra@duocuc.cl>
2026-06-11 16:39:52 -0400

    1.1 s3 backend

--- EA1/ACT1.1/provider.tf
+++ EA1/ACT1.1/provider.tf
@@ -1,9 +1,20 @@
-terraform {
+/* terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.0"
     }
+  }
+} */
+
+# Backend en s3
+
+terraform {
+  backend "s3" {
+    bucket = "bucket-terraform-611"
+    key = "terraform.stfstate"
+    region = "us-east-1"
+    use_lockfile = true
   }
 }
 
